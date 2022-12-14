d2abf0bdc6885de0a4374b5df6bcee82c4d6c174
OlegMamaev <oleg.mamaew2020@gmail.com>
2022-12-14 22:40:37 +0300

    new one

--- braniaclms/braniaclms/urls.py
+++ braniaclms/braniaclms/urls.py
@@ -1,7 +1,7 @@
 """braniaclms URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from mainapp import views
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', views.hello_world, name='hello_world'),
+    path('', RedirectView.as_view(url='mainapp/')),
+    path('mainapp/', include('mainapp.urls'))
 ]
--- braniaclms/mainapp/views.py
+++ braniaclms/mainapp/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def hello_world(request):
-    return HttpResponse("hello world!"),
+class ContactView(TemplateView):
+    template_name = 'mainapp/contact.html'
+
+
+class CoursesListView(TemplateView):
+    template_name = 'mainapp/courses_list.html'
+
+
+class DocSiteView(TemplateView):
+    template_name = 'mainapp/doc_site.html'
+
+
+class IndexView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'mainapp/login.html'
+
+
+class NewsView(TemplateView):
+    template_name = 'mainapp/news.html'
